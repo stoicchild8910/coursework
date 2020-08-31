e413eb1a8f9b0a29d97f7d8ff777470f865e4cf1
stoicchild <stoicchild8910@gmail.com>
2020-08-31 22:40:25 +0900

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# coursework
-coursework to share :)
---
+++ precache-manifest.28b338d1d68652bad75983b9f230e1cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc076aa8e0ed137ff7be502ab8e1e2b6",
+    "url": "/coursework/index.html"
+  },
+  {
+    "revision": "cd9b54b297df37a06328",
+    "url": "/coursework/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0384ec9f75034b515520",
+    "url": "/coursework/static/js/2.349f4d92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coursework/static/js/2.349f4d92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd9b54b297df37a06328",
+    "url": "/coursework/static/js/main.2ee4845e.chunk.js"
+  },
+  {
+    "revision": "4149baf204fbef43ef7d",
+    "url": "/coursework/static/js/runtime-main.c7738b8a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coursework/static/media/logo.5d5d9eef.svg"
+  }
+]);

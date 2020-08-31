4af844d499df360983f1da8fb068afe96af7d094
stoicchild <stoicchild8910@gmail.com>
2020-08-31 19:14:53 +0900

    Updates

---
+++ precache-manifest.12c03a02f002e062d25272a1552039d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e71744e074d881a2916bfed3f7e7f3b",
+    "url": "/coursework/movieweb/index.html"
+  },
+  {
+    "revision": "de08d67ab106286cf2e6",
+    "url": "/coursework/movieweb/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "636ba3efd519e52e5be7",
+    "url": "/coursework/movieweb/static/js/2.f6c5811a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coursework/movieweb/static/js/2.f6c5811a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de08d67ab106286cf2e6",
+    "url": "/coursework/movieweb/static/js/main.b62e6342.chunk.js"
+  },
+  {
+    "revision": "78a36e9a0fe831c95f88",
+    "url": "/coursework/movieweb/static/js/runtime-main.180d03a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/coursework/movieweb/static/media/logo.5d5d9eef.svg"
+  }
+]);
